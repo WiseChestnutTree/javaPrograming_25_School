fb3219800f274b8c8a9f345116e161402bf00ee7
yool <syhj3238@naver.com>
2025-03-28 16:56:21 +0900

    chap3 input & school_ex01 class set

--- JaPro_Ch03/src/chap3/Input01.java
+++ JaPro_Ch03/src/chap3/Input01.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 public class Input01 {
    public static void main(String[] args) {
 
-      Scanner s1 = new Scanner(System.in);
-      System.out.println("������ �Է��ϱ�");
-      int n = s1.nextInt();
-
-      System.out.println("�Ǽ��� �Է��ϱ�");
-      double db = s1.nextDouble();
-
-      System.out.println("������ :" + n);
-      System.out.println("�Ǽ��� :" + db);
+//      Scanner s1 = new Scanner(System.in);
+//      System.out.println("������ �Է��ϱ�");
+//      int n = s1.nextInt();
+//
+//      System.out.println("�Ǽ��� �Է��ϱ�");
+//      double db = s1.nextDouble();
+//
+//      System.out.println("������ :" + n);
+//      System.out.println("�Ǽ��� :" + db);
+	   Scanner str = new Scanner(System.in);
+	   String s = str.nextLine();
+	   System.out.println("값: "+s);
    
     }
 }
