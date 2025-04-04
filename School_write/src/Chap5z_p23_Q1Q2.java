0b9f2393f488af270127c4c7c24086e6c8377655
yool <syhj3238@naver.com>
2025-04-04 17:08:16 +0900

    04. 04 school

--- School_write/src/Chap5z_p23_Q1Q2.java
+++ School_write/src/Chap5z_p23_Q1Q2.java
@@ -57,15 +57,16 @@ public class Chap5z_p23_Q1Q2 {
 		
 		money = scan.nextInt();
 		System.out.println("구조조정으로 인해 월급이 20% 삭감되었습니다.");
-		double div = c.devide(money, 20);
+		double div = c.multi(money, 0.2);
 		money = c.minus(money, div);
+		System.out.println(money);
 		System.out.println("삭감된 월급 : "+money+"만원입니다.");
 		
-		System.out.println("삭감된 연봉 : "+c.salary2(money));
+		System.out.println("삭감된 연봉 : "+c.salary2(money)+"만원입니다.");
 	}
 	
 	
-	public static void main(String args) {
+	public static void main(String[] args) {
 		Chap5z_p23_Q1Q2 c = new Chap5z_p23_Q1Q2();
 		Scanner scan = new Scanner(System.in);
 //		Q1
@@ -75,6 +76,31 @@ public class Chap5z_p23_Q1Q2 {
 //		Q2
 		c.calcMain2(scan);
 		
+		c.exMatrix();
+	}
+	
+	
+	void exMatrix() {
+		int[] score = new int[] {100, 96, 88, 78, 65};
+		
+		int[] num = {12, 22, 32, 42, 52};
+		
+		for (int i = 0; i<5; i++) {
+			score[i] = 10 + 1*i;
+		}
+		
+		for (int i=0; i<5; i++) {
+			System.out.println("score 배열 : "+score[i]);
+			System.out.println("num 배열 : "+num[i]);
+		}
+		
+		int[][] grade = new int[5][3];
+		
+		for (int i=0; i<5; i++) {
+			for (int j=0; j<3; j++) {
+				grade[i][j]	= 10*(i+1) + (j+1); 
+			}
+		}
 	}
 	
 }
